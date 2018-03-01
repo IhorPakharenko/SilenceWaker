96b2bfc8ebd7bdda42870f44c6a17175ec8fc9ed
AlexStorchak <alexsstorchak@gmail.com>
2018-03-01 21:16:53 +0200

    Prepare to EditAlarm

---
+++ app/src/main/java/com/example/isao/silentwaker/editAlarmActivity/EditAlarmActivity.kt
@@ -0,0 +1,13 @@
+package com.example.isao.silentwaker.editAlarmActivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.isao.silentwaker.R
+
+class EditAlarmActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_alarm)
+    }
+}
